3fb9f47178e709f1ff5ba6618e0a3f89094c2de4
CuteHat <jimmysinjaradze@gmail.com>
2021-01-14 01:12:04 +0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PostListComponent} from "./posts/post-list/post-list.component";
+import {AddPostComponent} from "./posts/add-post/add-post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'post', component: PostListComponent},
+  {path: 'post/new', component: AddPostComponent},
+  {path: '', redirectTo: 'post', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
