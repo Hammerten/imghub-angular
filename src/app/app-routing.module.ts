9a58fe503a39f8bf882cc2516412a5485cdd53f2
CuteHat <jimmysinjaradze@gmail.com>
2021-01-15 01:16:07 +0400

    add authguard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import {AddPostComponent} from './posts/add-post/add-post.component';
 import {LoginFormComponent} from './login-form/login-form.component';
 import {RegisterFormComponent} from './register-form/register-form.component';
 import {ViewPostComponent} from "./posts/view-post/view-post.component";
+import {AuthGuardService} from "./services/auth-guard.service";
 
 const routes: Routes = [
   {path: 'post', component: PostListComponent},
-  {path: 'post/new', component: AddPostComponent},
+  {path: 'post/new', component: AddPostComponent, canActivate: [AuthGuardService]},
   {path: 'post/:id', component: ViewPostComponent},
   {path: 'login', component: LoginFormComponent},
   {path: 'register', component: RegisterFormComponent},
